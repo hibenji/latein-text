d2baf7261c7f1f5fc3444296ad5d2a3e9691292a
Benji <65447501+hibenji@users.noreply.github.com>
2022-10-17 20:07:05 +0000

    did some styling

--- output.php
+++ output.php
@@ -9,66 +9,76 @@
   
 <?php
 
-$words = $_POST['words'];
-
-// echo $words;
-
-// split the words into an array
-$words = explode(' ', $words);
-
-echo "<div class='match'>";
-
-// loop through the array and print each word
-foreach ($words as $word) {
-
-  // check api https://www.latein.me/_API_QUE.php?q=<word>
-  $url = 'https://www.latein.me/_API_QUE.php?q=' . $word;
-
-  // get the json data
-  $data = file_get_contents($url);
-  // nox (Substantiv)///CgD///noctis, f.///Nacht///Lokativ Singular, Femininum%%%Ablativ Singular, Femininum%%%Dativ Singular, Femininum%%%///$$$
-  // split the data into an array
+$input = $_POST['words'];
 
 
-  $data_new = explode('$$$', $data);
-
-  $simple = explode('///', $data);
-
-  
-  ?>
-
-  <div class="word" title="<?php  echo $simple[3];  ?>"><?php  echo $word ?>
-    <span class="tooltip">
-  <?php
+// slit each line into an array
+$lines = preg_split("/\r\n|\n|\r/", $input);
 
 
-  foreach ($data_new as $type) {
+foreach ($lines as $words) {
+
+  echo "<div class='columns'>";
+
+  // split the words into an array
+  $words = explode(' ', $words);
 
 
-    $type = explode('///', $type);
+  // loop through the array and print each word
+  foreach ($words as $word) {
 
-    // // word one
-    // echo $type[3];
 
-    // check if $type[0] is empty
-    if (empty($type[0])) {
-      // do nothing
-    } else {
-      echo $type[0];
-      echo ": ";
-      echo $type[3];
-      echo "<br>";
+    // check api https://www.latein.me/_API_QUE.php?q=<word>
+    $url = 'https://www.latein.me/_API_QUE.php?q=' . $word;
+
+    // get the json data
+    $data = file_get_contents($url);
+    // nox (Substantiv)///CgD///noctis, f.///Nacht///Lokativ Singular, Femininum%%%Ablativ Singular, Femininum%%%Dativ Singular, Femininum%%%///$$$
+    // split the data into an array
+
+
+    $data_new = explode('$$$', $data);
+
+    $simple = explode('///', $data);
+
+    
+    ?>
+
+    <div class="column">
+    <div class="word" title="<?php  echo $simple[3];  ?>"><?php  echo $word ?>
+      <span class="box">
+    <?php
+
+
+    // loop through the array and print each translated word
+    foreach ($data_new as $type) {
+
+      $type = explode('///', $type);
+
+      // // word one
+      // echo $type[3];
+
+      // check if $type[0] is empty
+      if (empty($type[0])) {
+        // do nothing
+      } else {
+        echo $type[0];
+        echo ": ";
+        echo $type[3];
+        echo "<br>";
+      }
+
     }
+    echo "</span>";
+
+    echo "</div><br>";
+    echo "</div>";
 
   }
-  echo "</span>";
 
-  echo "</div><br>";
+  echo "</div>";
 
 }
-
-echo "</div>";
-
 ?>
     </div>
   </section>
